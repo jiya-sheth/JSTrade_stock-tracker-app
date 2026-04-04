7743b424599e2625e2b722ba891adbe16f35b8ea
Jiya Sheth <jiyasheth246@gmail.com>
2026-04-04 14:22:39 +0530

    implement header

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,18 @@
 "use client";
 import {Button} from "@/components/ui/button";
+import TradingViewWidget from "@/components/ui/TradingViewWidget";
 
 const Home = () => {
     return (
         <div className="flex min-h-screen home-wrapper">
-            Home
+            <section className="grid w-full gap-8 home-section">
+                <div className="md:col-span-1 xl:col-span-1">
+                    <TradingViewWidget
+                        title="Market Overview"
+                        scriptUrl="https://s3.tradingview.com/external-embedding/embed-widget-market-overview.js"
+                    />
+                </div>
+            </section>
         </div>
     )
 }
