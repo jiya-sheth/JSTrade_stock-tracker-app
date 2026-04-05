466c52fd0e2f242899bd08fe9858159e7495826e
Jiya Sheth <jiyasheth246@gmail.com>
2026-04-05 16:16:21 +0530

    Implement home page with TradingView widgets

--- hooks/useTradingViewWidget.tsx
+++ hooks/useTradingViewWidget.tsx
@@ -7,16 +7,17 @@ const useTradingViewWidget = (scriptUrl: string, config: Record<string, unknown>
     useEffect( () => {
 
         if (!containerRef.current) return;
-        if (!containerRef.current.dataset.loaded) return;
-        containerRef.current.innerHTML = '<div class ="tradingview-widget-container__widget" stlye = "width: 100%; height: ${height}px;"></div>';
+        if (containerRef.current.dataset.loaded) return;
+        containerRef.current.innerHTML = '<div class ="tradingview-widget-container__widget" style = "width: 100%; height: ${height}px;"></div>';
 
         const script = document.createElement("script");
         script.src = scriptUrl;
         script.async = true;
         script.innerHTML = JSON.stringify(config);
 
-        containerRef.current.appendChild(script);
         containerRef.current.dataset.loaded = 'true';
+        containerRef.current.appendChild(script);
+
 
         return () => {
             if(containerRef.current) {
